3f2917f065f722957dc551e09d0df9d21f183fb8
Stéphane Payrard <cognominal@gmail.com>
2023-06-03 14:13:31 +0200

    draggable polyform. drop TBD. no touch support
    Disabled ssr and prerender because I had message about `document` not accessible.
     I thought svelte was using jsdom for prerendering.
    
    Other issue
    The svelte-drag-drop-touch refuses to load and I have
    commented out the import.
    
    The message:
    Failed to resolve entry for package "svelte-drag-drop-touch". The package may have incorrect main/module/exports specified in its package.json.

--- src/lib/Polyform.svelte
+++ src/lib/Polyform.svelte
@@ -1,31 +1,44 @@
 <script lang="ts">
-    import { calcPerimeter, perimeterPolylinePoints }  from '$lib/polyform'
-    import type { Tile, Int, Pos } from '$lib/polyform'
-    // import type { DragActions } from '$lib/drag'
-    export let tile : Tile
-    export let cellSize = 8
-    
-    let boardSize =  50 // tile[0].length *squareSize
-    // export let dragActions: DragActions
+	import { calcPerimeter, perimeterPolylinePoints } from '$lib/polyform';
+	import type { Tile, Int, Pos } from '$lib/polyform';
+	export let tile: Tile;
+	export let cellSize = 8;
+
+	let boardSize = 50; // tile[0].length *squareSize
+	// export let dragActions: DragActions
 </script>
-<svg width="{boardSize}" height="{boardSize}" viewBox="0 0 {boardSize} {boardSize}">
-        <polyline points={perimeterPolylinePoints(tile, cellSize)} class="pentamino draggable"/>
-</svg>
+
+<script lang="ts" context="module">
+	import { asDroppable } from 'svelte-drag-and-drop-actions';
+	// import DragDropTouch from 'svelte-drag-drop-touch';
+</script>
+
+<span use:asDroppable={{}}>
+	<svg width={boardSize} height={boardSize} viewBox="0 0 {boardSize} {boardSize}">
+		<polyline points={perimeterPolylinePoints(tile, cellSize)} class="pentamino draggable" />
+	</svg>
+</span>
+
 <!-- </span> -->
 
 <style>
-    .pentamino {
-        fill: lightblue;
-        stroke: red;
-        stroke-width: 2;
-    }
+	.pentamino {
+		fill: lightblue;
+		stroke: red;
+		stroke-width: 2;
+	}
 
-    .draggable {
-        cursor: move;
-    }
+	.draggable {
+		cursor: move;
+	}
 
-    .pentamino:hover {
-        fill: blue;
-    }
-    
+	.pentamino:hover {
+		fill: blue;
+	}
+    :global([draggable]) {
+    -webkit-touch-callout:none;
+    -ms-touch-action:none; touch-action:none;
+    -moz-user-select:none; -webkit-user-select:none; -ms-user-select:none; user-select:none;
+  }
 </style>
+
