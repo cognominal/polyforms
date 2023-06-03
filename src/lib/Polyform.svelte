0763c536985c8301f02ee3020e83088a72ba40bf
Stéphane Payrard <cognominal@gmail.com>
2023-06-03 16:12:18 +0200

    handler simple/double click to change number of polyform instances
    flawed (when double click, see it as click and double click.
    Results into a noop.
    thx to Copilot chat

--- src/lib/Polyform.svelte
+++ src/lib/Polyform.svelte
@@ -1,41 +1,64 @@
-<script lang="ts">
-	import { calcPerimeter, perimeterPolylinePoints } from '$lib/polyform'
-	import type { Tile, Int, Pos, PBoard, TileInfo } from '$lib/polyform'
-	export let tileI: Int;
-	export let pBoard: PBoard;
-	export let cellSize = 8
-
-	let boardSize = 50; // tile[0].length *squareSize
-	// export let dragActions: DragActions
-
-	function tileFromI(pBoard: PBoard , i: Int): Tile {
-		return pBoard.tilesInfo[i].orients[0].matrix
-	}
-	function handleClick() {
-		
-	}
-	function handleKeypress() {
-		
-	}
-</script>
-
 <script lang="ts" context="module">
 	import { asDroppable } from 'svelte-drag-and-drop-actions';
 	// import DragDropTouch from 'svelte-drag-drop-touch';
 </script>
 
-<span use:asDroppable={{}} on:dblclick={handleClick} on:click={handleClick} on:keypress={handleKeypress}>
+<script lang="ts">
+	import { calcPerimeter, perimeterPolylinePoints } from '$lib/polyform';
+	import type { Tile, Int, Pos, PBoard, TileInfo } from '$lib/polyform';
+	export let tileI: Int;
+	export let pBoard: PBoard;
+	export let cellSize = 8;
+
+	let boardSize = 50; // tile[0].length *squareSize
+	// export let dragActions: DragActions
+
+	function tileFromI(pBoard: PBoard, i: Int): Tile {
+		return pBoard.tilesInfo[i].orients[0].matrix;
+	}
+
+	let clicked = false;
+
+	function handleClick(evt: MouseEvent) {
+		if (!clicked) {
+			clicked = true;
+			setTimeout(() => {
+				if (clicked) {
+					clicked = false;
+					// handle single click
+					pBoard.tilesLeft[tileI]++;
+				}
+			}, 300);
+		} else {
+			clicked = false;
+			evt.preventDefault();
+			// handle double click
+			pBoard.tilesLeft[tileI]--;
+		}
+	}
+	function handleKeypress() {}
+</script>
+
+<span
+	use:asDroppable={{}}
+	on:dblclick={handleClick}
+	on:click={handleClick}
+	on:keypress={handleKeypress}
+>
 	<svg width={boardSize} height={boardSize} viewBox="0 0 {boardSize} {boardSize}">
-		<polyline points={perimeterPolylinePoints(tileFromI(pBoard, tileI), cellSize)} class="pentamino draggable" />
+		<polyline
+			points={perimeterPolylinePoints(tileFromI(pBoard, tileI), cellSize)}
+			class="pentamino draggable"
+		/>
 	</svg>
 </span>
-<span class="nrInstances">{pBoard.tilesLeft[tileI]}</span>
+<span class="nrInstances" id="inr{tileI}">{pBoard.tilesLeft[tileI]}</span>
 
 <!-- </span> -->
 
 <style>
 	.nrInstances {
-		margin: auto
+		margin: auto;
 	}
 
 	.pentamino {
@@ -51,10 +74,13 @@
 	.pentamino:hover {
 		fill: blue;
 	}
-    :global([draggable]) {
-    -webkit-touch-callout:none;
-    -ms-touch-action:none; touch-action:none;
-    -moz-user-select:none; -webkit-user-select:none; -ms-user-select:none; user-select:none;
-  }
+	:global([draggable]) {
+		-webkit-touch-callout: none;
+		-ms-touch-action: none;
+		touch-action: none;
+		-moz-user-select: none;
+		-webkit-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
 </style>
-
