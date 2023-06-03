2b86ea05d6e4ecbb9df3b7c063cbd0a6285c4ab4
Stéphane Payrard <cognominal@gmail.com>
2023-06-03 07:37:27 +0200

    display the pentaminos in TileBoard

--- src/routes/Tests/Board/+page.svelte
+++ src/routes/Tests/Board/+page.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
 	import { calcPolyominos, setPBoard } from '$lib/polyform';
-	import type { TileInfo } from '$lib/polyform';
+	import type { TileInfo, PBoard } from '$lib/polyform';
 	import Pentamino from '$lib/Polyform.svelte';
 	import Grid from '$lib/Grid.svelte';
 	import TileBoard from '$lib/TileBoard.svelte';
   import PPBoard from '$lib/PPBoard.svelte';
 	let ti: TileInfo[][] = calcPolyominos(6);
 	let pentaminoTi = ti[4];
-	let tile = pentaminoTi[0].orients[0].matrix;
+  let tile = pentaminoTi[0].orients[0].matrix;
+	let tiles = pentaminoTi.map(ti => ti.orients[0].matrix);
 	let w = 8;
 	let h = 8;
 	let cellSize = 8;
 	let boardSize = w * cellSize;
 
-  let pBoard : PPBoard= setPBoard(10, 6, pentaminoTi);
+  let pBoard : PBoard= setPBoard(10, 6, pentaminoTi);
 </script>
 
 <div class="container" style="height:{boardSize}px;">
 	<Grid  squareSize={cellSize} matrix={tile} active={false} />
-	<TileBoard tiles={[tile]} {cellSize} />
 </div>
 <hr />
 <div>
-  <TileBoard tiles={[tile]} {cellSize} />
+  <TileBoard {tiles} {cellSize} />
   <PPBoard {pBoard} />
 </div>
 <style>
