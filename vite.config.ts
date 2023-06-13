790aa89ddd6361c6de28c93709ff153ca42c12bc
Stéphane Payrard <cognominal@gmail.com>
2023-06-13 21:25:06 +0200

    try to autoimport components. No success yet

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import autoImport from 'sveltekit-autoimport';
 
+autoImport({
+	components: { name: './src/lib', flat: true }
+  })
+  
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
