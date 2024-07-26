61b84d3ba2f5695f3385aa90f0d3c23782ace65b
Stéphane Payrard <cognominal@gmail.com>
2024-07-26 07:19:51 +0200

    Added support for mdsvex
    cargo culted it from https://github.com/josh-collinsworth/sveltekit-blog-starter

---
+++ src/routes/about/+page.md
@@ -0,0 +1,3 @@
+This is an app dealing with [Polyform](https://en.wikipedia.org/wiki/Polyform) tiles.
+You can define a board and place tiles on it.
+You can use predefined tiles or define your own.
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,32 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
+import { mdsvex } from 'mdsvex'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+		// Ensures both .svelte and .md files are treated as components (can be imported and used anywhere, or used as pages)
+		extensions: ['.svelte', '.md'],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			// The default mdsvex extension is .svx; this overrides that.
+			extensions: ['.md'],
+
+			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+			rehypePlugins: [
+				rehypeSlug,
+				rehypeAutolinkHeadings,
+			],
+		}),
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
